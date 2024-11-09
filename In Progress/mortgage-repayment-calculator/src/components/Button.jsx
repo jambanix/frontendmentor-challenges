843341152d290e175ba3cd6d76cf7a1779f5ba97
jambanix <jambanix@pm.me>
2024-11-09 10:20:08 +0000

    remove in progress challenge

--- In Progress/mortgage-repayment-calculator/src/components/Button.jsx
+++
@@ -1,6 +0,0 @@
-
-export const Button = ({children, disabled, onClick}) => {
-  return (
-    <button onClick={onClick} disabled={disabled} className={`rounded-full bg-lime transition-all text-slate-900 max-w-[314px] h-12 hover:contrast-125 ${disabled ? "hidden" : "visible"}`}>{children}</button>
-  )
-}
--- In Progress/mortgage-repayment-calculator/src/components/Layout/PageContainer.jsx
+++
@@ -1,9 +0,0 @@
-export const PageContainer = ({ children }) => {
-  return (
-    <main>
-      <div className="flex items-center justify-center w-full min-h-screen bg-slate-300">
-        {children}
-      </div>
-    </main>
-  );
-};
